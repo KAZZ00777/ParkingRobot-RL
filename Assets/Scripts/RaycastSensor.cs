265c8d0e4342ccbcd7a7fae58216da7e07b23800
Kazuki Shoji <167762624+KAZZ00777@users.noreply.github.com>
2026-06-03 17:23:50 +0900

    Refactor RaycastSensor for improved readability

--- Assets/Scripts/RaycastSensor.cs
+++ Assets/Scripts/RaycastSensor.cs
@@ -1,51 +1,69 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class RaycastSensor : MonoBehaviour
 {
-public float sensorLength = 20f;
+    public float sensorLength = 10f;
 
-public float[] distances = new float[8];
+    public float[] distances = new float[8];
 
-void Update()
-{
-CastSensors();
-}
+    public float sensorHeight = 0.5f;
 
-void CastSensors()
-{
-Vector3[] directions =
-{
-transform.forward,
-(transform.forward + transform.right).normalized,
-transform.right,
-(-transform.forward + transform.right).normalized,
--transform.right,
-(transform.forward - transform.right).normalized,
--transform.forward,
-(-transform.forward - transform.right).normalized
-};
+    void Update()
+    {
+        CastSensors();
+    }
 
-for(int i = 0; i < directions.Length; i++)
-{
-RaycastHit hit;
+    void CastSensors()
+    {
+        Vector3[] directions =
+        {
+            transform.forward, //front
+            transform.right,//right
+            -transform.right, //left
+            -transform.forward,//back   
+            (transform.forward + transform.right).normalized,//front right
+            (transform.forward - transform.right).normalized,//front left
+            (-transform.forward + transform.right).normalized,//back right
+            (-transform.forward - transform.right).normalized//back left
+        };
 
-if(Physics.Raycast(transform.position, directions[i], out hit, sensorLength))
-{
-distances[i] = hit.distance;
+        String[] sensorNames =
+        {
+            "Front",
+            "Right",
+            "Left",
+            "Back", 
+            "FrontRight",
+            "FrontLeft",
+            "BackRight",
+            "BackLeft"
+        };
 
-Debug.DrawRay(transform.position, directions[i] * hit.distance, Color.red);
-}
-else
-{
-distances[i] = sensorLength;
+        for(int i = 0; i < directions.Length; i++)
+        {
+            RaycastHit hit;
 
-Debug.DrawRay(transform.position, directions[i] * sensorLength, Color.green);
-}
+            Vector3 origin = transform.position + Vector3.up * sensorHeight;
 
-Debug.Log("i = " + distances[i]);
+            if(Physics.Raycast(origin, directions[i], out hit, sensorLength))
+            {
+                distances[i] = hit.distance;
 
-}
+                Debug.DrawRay(origin, directions[i] * hit.distance, Color.red);
+            }
+            else
+            {
+                distances[i] = sensorLength;
 
-}
-}
+                Debug.DrawRay(origin, directions[i] * sensorLength, Color.green);
+            }
+
+            Debug.Log(sensorNames[i] + " = " + distances[i]);
+
+        }
+
+        
+    }
+}   
